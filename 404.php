aacaf7e402756bc19494c34d9f6d345b5871345f
Jana Fisdel <janafisdel@gmail.com>
2021-08-21 19:52:10 -0300

    pagina error 404

---
+++ 404.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="scss/newstyles.css" />
+
+    <title> Página no disponible</title>
+</head>
+
+<body>
+    <main id=error__flex>
+        <aside >
+            <img id=error__logo src="imagenes/logo_tour.jpg" alt="logo">
+        </aside>
+
+        <section id="error__text">
+                <h1>Lo sentimos!</h1>
+                <h2>Página no encontrada. </h2>
+
+                <p>Para visitar nuestros destinos turisticos utiliza el siguiente enlace.</p>
+
+                <a href="index.html"><button type="button" class="btn btn-primary">Ir a Touristas</button></a>
+        
+        </section>
+    </main>
+
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
